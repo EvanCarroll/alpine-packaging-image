119658d8b4946de91f9878726649019a2c568ae2
Evan Carroll <me@evancarroll.com>
2021-02-20 01:39:14 -0600

    fix bug with quotes

--- run.sh
+++ run.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
+
+# won't work, quoting problems
+# https://github.com/containers/podman/issues/9446
+# --env-file=./.env
+
+set -o allexport
+. ./.env
+set +o allexport
+
 podman run                        \
 	-ti                             \
 	-v "$PWD/aports:/root/aports"   \
 	--group-add=abuild              \
+	--env="HOST_*"                  \
 	--cap-add=cap_net_raw           \
-	--env-file=./.env               \
 	localhost/alpine-packaging-image /bin/sh
